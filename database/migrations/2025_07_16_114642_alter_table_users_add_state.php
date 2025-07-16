c5d82e3dce81937599666f979c18f847f622643e
Adilson19 <adilsonsousaas82@gmail.com>
2025-07-16 12:26:16 +0100

    feat: criando as migrations e configurando elas

---
+++ database/migrations/2025_07_16_114642_alter_table_users_add_state.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\State;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //  Adicionando o relacionamento entre a tabela User com a tabela State
+            $table->foreignIdFor(State::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //  Fazendo o crontrario (Apagando a chave estrangeira e a coluna)
+            $table->dropForeign(['state_id']);
+            $table->dropColumn('state_id');
+        });
+    }
+};
