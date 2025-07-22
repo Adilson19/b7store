2a16f8ec433e145f026c1ea406cb721495758bf9
Adilson19 <adilsonsousaas82@gmail.com>
2025-07-22 09:40:36 +0100

    feat: comunicando a view com o controller, atraves de uma rota de um controller de autenticacao para regras de negocio

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +18,8 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/register', function(){
-    return view('auth.register');
-})->name('register');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'register_action'])->name('register_action');
 
 Route::get('/login', function(){
     return view('auth.login');
