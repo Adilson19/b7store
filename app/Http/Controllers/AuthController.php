e4e211e15cfb585205efb0a94a26561d6e453635
Adilson19 <adilsonsousaas82@gmail.com>
2025-07-29 20:51:08 +0100

    feat: colocando as bibliotecas para utilizar a autenticacao

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Http\Requests\RegisterRequest;
 
@@ -23,7 +24,8 @@ class AuthController extends Controller
         $user = User::create($userData);
 
         Auth::loginUsingId($user->id);
-        
+        return redirect()->route('select-state');
+        dd($user);
 
     }
 
